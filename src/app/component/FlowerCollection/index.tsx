ec2d0cd1f84eb9d9b0347ba03c20337d66c73ff7
fid <femiomoj2001@gmail.com>
2025-10-04 12:10:34 +0100

    update

--- src/app/component/FlowerCollection/index.tsx
+++ src/app/component/FlowerCollection/index.tsx
@@ -64,7 +64,8 @@ export default function FlowerCollection() {
           </h1>
         </div>
       </div>
-      <div className="relative mx-auto max-w-[1200px]  py-4 px-1">
+
+      <div className="relative mx-auto max-w-[1200px] px-4 pt-4 sm:py-14 sm:px-1">
         <Image
           src="/flowers.webp"
           alt="flower"
@@ -74,7 +75,7 @@ export default function FlowerCollection() {
         />
         <div className="relative mb-4">
           <motion.h1
-            className="text-center text-secondary-100! cursor-pointer  text-shadow-black/30 font-playfair! font-medium text-3xl! sm:text-4xl!"
+            className="text-center text-secondary-100! cursor-pointer  text-shadow-black/30 font-playfair! font-medium text-2xl! sm:text-4xl!"
             drag="x"
             dragConstraints={{ left: 0, right: 0 }}
             dragElastic={0.2}
@@ -85,20 +86,20 @@ export default function FlowerCollection() {
           >
             Share Well Wishes
           </motion.h1>
-          <h2 className="text-secondary-100! text-center text-lg sm:text-xl">
+          <h2 className="text-secondary-100! text-center text-md sm:text-xl">
             Say &apos;Get well soon&apos; with this special bouquet flowers.
           </h2>
           <span className="block text-center mt-6">
-            <a
+            {/* <a
               href="#"
               className="bg-amber-200 py-3 px-4 font-semibold text-amber-900"
             >
               Shop More
-            </a>
+            </a> */}
           </span>
         </div>
 
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 auto-cols-fr gap-4 sm:px-12 sm:gap-12">
+        <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-5 auto-cols-fr gap-4 sm:px-12 sm:gap-x-12 sm:gap-y-0">
           {[
             {
               src: "flora1.avif",
@@ -125,17 +126,17 @@ export default function FlowerCollection() {
               <div className="relative space-y-2" key={_.src}>
                 <img
                   src={`/${_.src}`}
-                  className="aspect-squares drop-shadow-2xl"
+                  className="max-sm:w-28 mx-auto aspect-squares drop-shadow-2xl"
                 />
                 <div className="text-center">
                   <a
-                    className="text-center! inline-block! bg-amber-300 px-2 py-1 text-amber-900 font-playfair rounded text-sm"
+                    className="text-center! inline-block! bg-amber-300 px-2 py-1 text-amber-900 font-playfair rounded text-[12px]! sm:text-sm! max-sm:font-medium"
                     href="#"
                   >
                     Buy now
                   </a>
                 </div>
-                <p className="wrap-normal text-center font-playfair! font-extralight! text-[16px]! sm:text-[18px]!">
+                <p className="wrap-normal text-center font-playfair! font-extralight! text-[14px]! sm:text-[16px]!">
                   {_.name}
                 </p>
               </div>
@@ -144,7 +145,7 @@ export default function FlowerCollection() {
         </div>
       </div>
 
-      <div className="relative mx-auto max-w-[1200px]  py-4 px-1">
+      <div className="relative mx-auto max-w-[1200px] px-4 pt-4 sm:py-14 sm:px-1">
         <Image
           src="/flowers-1.webp"
           alt="flower"
@@ -154,7 +155,7 @@ export default function FlowerCollection() {
         />
         <div className="relative mb-4">
           <motion.h1
-            className="text-center text-secondary-100! cursor-pointer  text-shadow-black/30 font-playfair! font-medium text-3xl! sm:text-4xl!"
+            className="text-center text-secondary-100! cursor-pointer  text-shadow-black/30 font-playfair! font-medium text-2xl! sm:text-4xl!"
             drag="x"
             dragConstraints={{ left: 0, right: 0 }}
             dragElastic={0.2}
@@ -165,17 +166,20 @@ export default function FlowerCollection() {
           >
             Share Well Wishes
           </motion.h1>
-          <h2 className="text-secondary-100! text-center text-lg sm:text-xl">
+          <h2 className="text-secondary-100! text-center text-md sm:text-xl">
             Say &apos;Get well soon&apos; with this special bouquet flowers.
           </h2>
           <span className="block text-center mt-6">
-            <a href="#" className="bg-amber-700 py-3 px-4 text-amber-50">
+            {/* <a
+              href="#"
+              className="bg-amber-200 py-3 px-4 font-semibold text-amber-900"
+            >
               Shop More
-            </a>
+            </a> */}
           </span>
         </div>
 
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 auto-cols-fr gap-4 sm:px-12 sm:gap-12">
+        <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-5 auto-cols-fr gap-4 sm:px-12 sm:gap-x-12 sm:gap-y-0">
           {[
             {
               src: "flora1.avif",
@@ -202,17 +206,17 @@ export default function FlowerCollection() {
               <div className="relative space-y-2" key={_.src}>
                 <img
                   src={`/${_.src}`}
-                  className="aspect-squares drop-shadow-2xl"
+                  className="max-sm:w-28 mx-auto aspect-squares drop-shadow-2xl"
                 />
                 <div className="text-center">
                   <a
-                    className="text-center! inline-block! border border-amber-700 px-2 py-1 text-amber-700 font-playfair rounded text-sm"
+                    className="text-center! inline-block! bg-amber-300 px-2 py-1 text-amber-900 font-playfair rounded text-[12px]! sm:text-sm! max-sm:font-medium"
                     href="#"
                   >
                     Buy now
                   </a>
                 </div>
-                <p className="wrap-normal text-center font-playfair! font-extralight! text-[16px]! sm:text-[18px]!">
+                <p className="wrap-normal text-center font-playfair! font-extralight! text-[14px]! sm:text-[16px]!">
                   {_.name}
                 </p>
               </div>
@@ -221,7 +225,7 @@ export default function FlowerCollection() {
         </div>
       </div>
 
-      <div className="relative mx-auto max-w-[1200px]  py-12 px-1">
+      <div className="relative mx-auto max-w-[1200px] px-4 pt-4 sm:py-14 sm:px-1">
         <Image
           src="/exclusives.webp"
           alt="flower"
@@ -231,7 +235,7 @@ export default function FlowerCollection() {
         />
         <div className="relative mb-4">
           <motion.h1
-            className="text-center text-secondary-100! cursor-pointer  text-shadow-black/30 font-playfair! font-medium text-3xl! sm:text-4xl!"
+            className="text-center text-secondary-100! cursor-pointer  text-shadow-black/30 font-playfair! font-medium text-2xl! sm:text-4xl!"
             drag="x"
             dragConstraints={{ left: 0, right: 0 }}
             dragElastic={0.2}
@@ -242,20 +246,20 @@ export default function FlowerCollection() {
           >
             Share Well Wishes
           </motion.h1>
-          <h2 className="text-secondary-100! text-center text-lg sm:text-xl">
+          <h2 className="text-secondary-100! text-center text-md sm:text-xl">
             Say &apos;Get well soon&apos; with this special bouquet flowers.
           </h2>
           <span className="block text-center mt-6">
-            <a
+            {/* <a
               href="#"
-              className="bg-purple-700 py-3 px-4 font-semibold text-purple-50"
+              className="bg-amber-200 py-3 px-4 font-semibold text-amber-900"
             >
               Shop More
-            </a>
+            </a> */}
           </span>
         </div>
 
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 auto-cols-fr gap-4 sm:px-12 sm:gap-12">
+        <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-5 auto-cols-fr gap-4 sm:px-12 sm:gap-x-12 sm:gap-y-0">
           {[
             {
               src: "flora1.avif",
@@ -282,17 +286,17 @@ export default function FlowerCollection() {
               <div className="relative space-y-2" key={_.src}>
                 <img
                   src={`/${_.src}`}
-                  className="aspect-squares drop-shadow-2xl"
+                  className="max-sm:w-28 mx-auto aspect-squares drop-shadow-2xl"
                 />
                 <div className="text-center">
                   <a
-                    className="text-center! inline-block! border border-purple-700 px-2 py-1 text-purple-700 font-playfair rounded text-sm"
+                    className="text-center! inline-block! bg-purple-500 px-2 py-1 text-purple-100 font-playfair rounded text-[12px]! sm:text-sm! max-sm:font-medium"
                     href="#"
                   >
                     Buy now
                   </a>
                 </div>
-                <p className="wrap-normal text-center font-playfair! font-extralight! text-[16px]! sm:text-[18px]!">
+                <p className="wrap-normal text-center font-playfair! font-extralight! text-[14px]! sm:text-[16px]!">
                   {_.name}
                 </p>
               </div>
--- src/app/component/ui/FlowerCard.tsx
+++ src/app/component/ui/FlowerCard.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { motion } from "motion/react";
+import { m, motion } from "motion/react";
 
 const MotionImage = motion(Image);
 
@@ -74,7 +74,10 @@ const MotionImage = motion(Image);
 
 export default function FlowerCard() {
   return (
-    <div className="rounded-lg! border-secondary-100/10 bg-[#EEE6CE] w-full space-y-2 flex flex-col relative hover:scale-105 transition-all duration-100 hover:shadow-lg">
+    <motion.div
+      whileHover="hover"
+      className="rounded-lg! border-secondary-100/10 bg-[#EEE6CE] w-full space-y-2 flex flex-col relative hover:scale-105 transition-all duration-100 hover:shadow-lg"
+    >
       <div className=" opacity-80 absolute bg-gradient-to-r from-40% from-purple-700 via-purple-600 via-60%  to-100% to-red-600 top-2 right-2 rounded-4xl z-[15]">
         <span className="text-[9px] text-white font-bold block p-1">
           Double Points
@@ -86,17 +89,20 @@ export default function FlowerCard() {
           alt="flower"
           objectFit="cover"
           fill={true}
-          whileHover={{
-            scale: 1.2,
+          variants={{
+            hover: {
+              scale: 1.2,
+            },
           }}
           transition={{
             type: "tween",
-            duration: 0.2,
+            ease: "linear",
+            duration: 0.1,
           }}
         />
       </motion.div>
 
-      <div className="py-2 px-2 flex gap-5 flex-col justify-between flex-1">
+      <div className="pt-1 pb-4 px-4 flex gap-5 flex-col justify-between flex-1">
         <div className="space-y-5">
           <div className="space-y-1">
             <div className=" flex justify-between items-end">
@@ -130,11 +136,29 @@ export default function FlowerCard() {
           {/* <h3 className="text-black! font-serif! text-xl! font-bold!">
                   $50
                 </h3> */}
-          <button className="text-sm! mt-4 text-secondary-100 font-semibold px-4 py-2 rounded-xs cursor-pointer  hover:text-secondary-100 transition ease-linear hover:border-[0.5px] border-secondary-100 duration-1s00 font-playfair! border-[0.5px] max-sm:self-start">
+          <motion.button
+            whileHover="hover"
+            initial="initial"
+            className="text-sm! mt-4 text-white font-semibold px-4 py-2 rounded-xs cursor-pointer transition ease-linear hover:border-[0.5px] border-secondary-100 duration-1s00 font-playfair! border-[0.5px] max-sm:self-start bg-secondary-100 relative overflow-hidden"
+          >
+            <motion.span
+              variants={{
+                initial: {
+                  x: "-100%",
+                },
+                hover: {
+                  x: "100%",
+                },
+              }}
+              transition={{
+                duration: 0.3,
+              }}
+              className="absolute inset-0 bg-linear-to-l from-transparent  via-white to-transparent opacity-40 pointer-events-none"
+            ></motion.span>
             View
-          </button>
+          </motion.button>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
